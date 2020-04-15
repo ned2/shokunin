bd4ecb61c85b9e3f31caf6b0991804becace27e2
Ned Letcher <nletcher@gmail.com>
2020-04-16 00:36:30 +1000

    Add missing help strings

--- src/luncher/cli.py
+++ src/luncher/cli.py
@@ -18,17 +18,17 @@ def main():
 
 @main.command(name="solve-one", help="Estimate probability of finding lunch for a given value of `p`.")
 @click.argument("proportion", type=float)
-@click.option("--samples", default=DEFAULT_SAMPLES, help="The number of populated offices to simulate")
-@click.option("--room-size", default=ROOM_SIZE, help="The number of ")
+@click.option("--samples", default=DEFAULT_SAMPLES, help="The number of populated offices to simulate.")
+@click.option("--room-size", default=ROOM_SIZE, help="The length in desks of a (square) office.")
 def solve_one(proportion, samples, room_size):
     prob = estimate_lunch_prob(proportion, samples)
     click.echo(f"Probability of finding lunch in a {room_size}x{room_size} room: {prob:.3f}")
 
 
 @main.command(name="solve-all", help="Get probability estimates for different increments of `p`.")
-@click.option("--samples", default=DEFAULT_SAMPLES, help="The number of populated offices to simulate")
-@click.option("--increments", default=DEFAULT_INCREMENTS)
-@click.option("--room-size", default=ROOM_SIZE)
+@click.option("--samples", default=DEFAULT_SAMPLES, help="The number of populated offices to simulate.")
+@click.option("--increments", default=DEFAULT_INCREMENTS, help="The amount to increment `p` by.")
+@click.option("--room-size", default=ROOM_SIZE, help="The length in desks of a (square) office.")
 def solve_all(samples, increments, room_size):
     df = get_probabilities(increments, samples)
     top = f"Number of samples for each p: {samples}"
